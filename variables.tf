17957ed01d84433ba95c4904635371b6e45cd0b9
vivekmokkarala <58174879+vivekmokkarala@users.noreply.github.com>
2025-06-30 16:27:35 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,23 @@
 variable "aws_region" {
-  default = "us-east-1"
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
 }
 
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
+variable "key_name" {
+  description = "Name of the key pair to use for the EC2 instance"
+  type        = string
+  default     = "jenkins-keys" # Update if different
 }

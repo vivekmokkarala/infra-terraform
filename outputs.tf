cb24a80626dc8ffd38b586a355dcd29157e38cbf
vivekmokkarala <58174879+vivekmokkarala@users.noreply.github.com>
2025-06-30 16:28:17 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public_subnet.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.web_server.public_ip
 }

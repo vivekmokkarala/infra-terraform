9ae2e2429cf94ded02e287702cae971bcb93998f
vivekmokkarala <58174879+vivekmokkarala@users.noreply.github.com>
2025-06-30 10:09:53 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "${var.aws_region}a"
+}
